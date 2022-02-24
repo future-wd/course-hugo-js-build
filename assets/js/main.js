e1060be6e8ddd003eb318eeb5a42b83e16b915be
sean-pwd <sean@powerfulwebdesign.com.au>
2022-02-23 20:45:54 -0500

    fix: complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-import * as bootstrap from 'bootstrap';
+// import the whole bootstrap library
+// import * as bootstrap from 'bootstrap';
+
+// import only the required the required parts of the library
+import { 
+  // Alert, 
+  // Button, 
+  // Carousel, 
+  Collapse, 
+  // Dropdown, 
+  // Modal, 
+  // Offcanvas, 
+  // Popover, 
+  // ScrollSpy, 
+  // Tab, 
+  // Toast, 
+  // Tooltip 
+} from 'bootstrap';
